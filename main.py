9815c1c5ffbaa194d9ce4ee83445ef00e36a2864
brengodoy <dia.godoybrenda@gmail.com>
2025-07-03 14:52:03 +0200

    api is working, modularized and validated the input

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
 import requests
-import json
+from typing import Dict
 
-def input_city_name():
-    city_name = input("Please enter the name of the city you'd like to know the weather of: ")
+def input_city_name() -> str:
+    while True:
+        city_name = input("Please enter the name of the city you'd like to know the weather of: ")
+        if any(cn.isdigit() for cn in city_name):
+            print("Numbers are not a valid input.")
+        else:
+            break
     return city_name
 
-def show_weather_info(data):
-    print(f"Here is the weather information for the city {city_name}: ")
-    print(f"Temperatue: {data["temperature"]}")
-    print(f"Description: {data["description"]}")
-    print(f"Humidity: {data["humidity"]}")
-    print(f"Wind: {data["wind"]}")
+def parse_weather_data(data: Dict[str,any]) -> Dict[str,any]:
+    return {
+		'city': data['name'],
+		'temp': data['main']['temp'],
+		'description': data['weather'][0]['description'],
+		'humidity': data['main']['humidity'],
+		'wind': data['wind']['speed']
+	}
+
+def show_weather_info(weather_info: Dict[str,any]) -> None:
+    print(f"Here is the weather information for {weather_info['city']}: ")
+    print(f"Temperatue: {weather_info['temp']}")
+    print(f"Description: {weather_info['description']}")
+    print(f"Humidity: {weather_info['humidity']}")
+    print(f"Wind: {weather_info['wind']}")
     
-def fetch_api_info(city_name):
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=2c826facbf92103fc989b8b9c5e9f19f&units=metric&lang=es"
-    #url1 = "https://weather.com/es-US/"
+def fetch_api_info(city_name: str):
+    api_key = "2c826facbf92103fc989b8b9c5e9f19f"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric&lang=es"
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
+        if data.get("cod") != 200:
+            print(f"Error de API: {data.get('message', 'Error desconocido')}")
+            return
         return data
     except requests.exceptions.RequestException as e:
         print(f"There was an issue trying to access to the weather information: {e}")
         return None
+    
+def main():
+    city_name = input_city_name()
+    data = fetch_api_info(city_name)
+    if data:
+        weather_info = parse_weather_data(data)
+        show_weather_info(weather_info)
+        
+if __name__ == "__main__":
+    main()
